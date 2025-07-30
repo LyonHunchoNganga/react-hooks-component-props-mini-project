783edfe01d81f277ea430b3c4f1eb765710e4093
lyonHunchoNganga <lyonnganga1@gmail.com>
2025-07-30 14:05:46 +0300

    commit

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,22 +1,10 @@
 import React from "react";
 
-function Article({ title, date = "January 1, 1970", preview, minutes }) {
-  const getReadingTime = () => {
-    if (!minutes) return "";
-    
-    if (minutes < 30) {
-      const cups = Math.ceil(minutes / 5);
-      return "☕️".repeat(cups) + ` ${minutes} min read`;
-    } else {
-      const boxes = Math.ceil(minutes / 10);
-      return "🍱".repeat(boxes) + ` ${minutes} min read`;
-    }
-  };
-
+function Article({ title, date = "January 1, 1970", preview }) {
   return (
     <article>
       <h3>{title}</h3>
-      <small>{date} • {getReadingTime()}</small>
+      <small>{date}</small>
       <p>{preview}</p>
     </article>
   );
